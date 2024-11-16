e4d8fcc4e0a34311b10e785e7c8f2cdefc7d48d9
Bohdan Pylypchenko <Bohdan314159yLypchenko@proton.me>
2024-11-16 18:22:19 +0200

    finish report

--- swarm-demo/main.cpp
+++ swarm-demo/main.cpp
@@ -1,11 +1,26 @@
 #include "pch.h"
 
 #include "automatic_optimization.h"
+#include "demo_run.h"
 #include "swarm_rndgen.h"
+
+#include "swarm_solution.h"
+#include "target_functions.h"
 
 #include <iostream>
 
 using swarm::core::FileRandomNumbersGenerator;
+using swarm::core::Solution;
+
+using swarm::demo::OptimizationParameters;
+using swarm::demo::demo_run_full;
+using swarm::demo::demo_run_value;
+using swarm::demo::griewank;
+using swarm::demo::rastrigin;
+using swarm::demo::rosenbrock;
+using swarm::demo::simple;
+using swarm::demo::ackley;
+
 using std::cout, std::endl;
 
 int main(void)
@@ -17,16 +32,70 @@ int main(void)
         return 1;
     }
 
-    cout << "=== griewank: ===" << endl;
-    swarm::demo::griewank_research();
-    cout << "=== rastringin: ===" << endl;
-    swarm::demo::rastrigin_research();
-    cout << "=== rosenbrock: ===" << endl;
-    swarm::demo::rosenbrock_research();
-    cout << "=== simple: ===" << endl;
-    swarm::demo::simple_research();
-    cout << "=== ackley: ===" << endl;
-    swarm::demo::ackley_research();
+    // uncomment to execute automatic optimization parameters selection (takes A LOT OF TIME!)
+    //cout << "=== griewank: ===" << endl;
+    //swarm::demo::griewank_research();
+    //cout << "=== rastringin: ===" << endl;
+    //swarm::demo::rastrigin_research();
+    //cout << "=== rosenbrock: ===" << endl;
+    //swarm::demo::rosenbrock_research();
+    //cout << "=== simple: ===" << endl;
+    //swarm::demo::simple_research();
+    //cout << "=== ackley: ===" << endl;
+    //swarm::demo::ackley_research();
+
+    // Optimal parameters. To ensure, execute automatic optimization.
+    //OptimizationParameters op
+    //{
+    //    2000, 4, 2, 20000, 1.2, 100, 0.01
+    //};
+
+    //cout << "=== griewank final run: ===" << endl;
+    //demo_run_full(op, griewank);
+
+    //cout << "=== rastringin final run: ===" << endl;
+    //demo_run_full(op, rastrigin);
+
+    //cout << "=== rosenbrock final run: ===" << endl;
+    //demo_run_full(op, rosenbrock);
+
+    //cout << "=== simple final run: ===" << endl;
+    //demo_run_full(op, simple);
+
+    //cout << "=== ackley final run: ===" << endl;
+    //demo_run_full(op, ackley);
+
+    OptimizationParameters ops[]
+    {
+        { 100, 4, 2, 20000, 1.2, 100, 0.01 },
+        { 200, 4, 2, 20000, 1.2, 100, 0.01 },
+        { 500, 4, 2, 20000, 1.2, 100, 0.01 },
+        { 1000, 4, 2, 20000, 1.2, 100, 0.01 },
+        { 1500, 4, 2, 20000, 1.2, 100, 0.01 },
+        { 2000, 4, 2, 20000, 1.2, 100, 0.01 },
+        { 4000, 4, 2, 20000, 1.2, 100, 0.01 },
+        { 8000, 4, 2, 20000, 1.2, 100, 0.01 },
+    };
+
+    cout << "=== griewank particle count research: ===" << endl;
+    for (auto& t : ops)
+        demo_run_value(t, griewank);
+
+    cout << "=== rastrigin particle count research: ===" << endl;
+    for (auto& t : ops)
+        demo_run_value(t, rastrigin);
+
+    cout << "=== rosenbrock particle count research: ===" << endl;
+    for (auto& t : ops)
+        demo_run_value(t, rosenbrock);
+
+    cout << "=== simple particle count research: ===" << endl;
+    for (auto& t : ops)
+        demo_run_value(t, simple);
+
+    cout << "=== ackley particle count research: ===" << endl;
+    for (auto& t : ops)
+        demo_run_value(t, ackley);
 
     FileRandomNumbersGenerator::free_random_numbers_source();
     return 0;
